4ab789a4d68e35d02b83074920f5cd70d2d03444
Đoàn Đức Huy <doanhuy1103@gmail.com>
2024-11-21 15:31:53 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,3 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\Account;
 
 Route::get('/', [AccountController::class, 'admin_login'])->name('admin.login');
-
-//Route::get('/admin', [AccountController::class, 'admin_login'])->name('admin_login');
-// Route::post('/admin/loginpost', [AccountController::class, 'loginPost'])->name('admin.loginPost');
-
-
-
-Route::middleware([Account::class])->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('view.all.users');
-    Route::get('/admin/dashboard', [AccountController::class, 'dashboard'])->name('admin.dashboard');
-});
