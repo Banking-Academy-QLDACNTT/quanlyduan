02ed08644ca9632ecdffc3157b325f5448f04db0
Đoàn Đức Huy <doanhuy1103@gmail.com>
2024-11-28 09:54:31 +0700

    import excel

--- app/Imports/AdminImport.php
+++ app/Imports/AdminImport.php
@@ -12,33 +12,34 @@ use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithUpserts;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class AdminImport implements ToModel, ToCollection, WithHeadingRow
+class AdminImport implements WithHeadingRow, ToCollection
 {
-    private $num = 0;
     /**
-    * @param collection $collection
+    * @param Collection \$rows
     *
-    * @return \Illuminate\Database\Eloquent\Model|null
+    * 
     */
-    public function collection(Collection $collection)
+    public function collection(collection $rows)
     {
-        //dd($collection);
-    }
-    public function model(array $row)
-    {
-        $this->num++;
-
         $adminUser = Auth::guard('admins')->user();
-
         $updateBy = DB::table('employees')->where('id', $adminUser->id)->pluck('employeeId')->first();
 
-        if ($this->num > 1) {
+        foreach ($rows as $row) {
+            if ($row->filter()->isEmpty()) {
+                continue; // Bỏ qua dòng trống
+            }
+
+            // Kiểm tra sự tồn tại của dữ liệu
             $count = Admin::where('username', $row['username'])->count();
             $count2 = Employee::where('phoneNumber', $row['phone_number'])->count();
 
             if (empty($count) && empty($count2)) {
+                $date = Date::excelToDateTimeObject($row['date_of_birth']);
+                $date_format = Carbon::instance($date)->format('Y-m-d');
+
                 $admin = Admin::create([
                     'username' => $row['username'],
                     'password' => Hash::make($row['password']),
@@ -47,10 +48,7 @@ class AdminImport implements ToModel, ToCollection, WithHeadingRow
                     'updated_at' => Carbon::now('Asia/Ho_Chi_Minh'),
                     'updateBy' => $updateBy
                 ]);
-                // Format date_of_birth
-                $dateOfBirth = Carbon::createFromFormat('d/m/Y', $row['date_of_birth'])->format('Y-m-d');
 
-                // Get department ID
                 $departmentId = DB::table('departments')
                     ->where('departmentName', $row['department'])
                     ->value('departmentId');
@@ -59,22 +57,17 @@ class AdminImport implements ToModel, ToCollection, WithHeadingRow
                     throw new \Exception("Department '{$row['department']}' does not exist.");
                 }
 
-                
-                    return new Employee([
-                        'name' => $row['name'],
-                        'phoneNumber' => $row['phone_number'],
-                        'dateOfBirth' => $dateOfBirth,
-                        'departmentId' => $departmentId,
-                        'sex' => strtolower($row['sex']) === 'Nam' ? 1 : 0,
-                        'id' => $admin->id,
-                        'created_at' => Carbon::now('Asia/Ho_Chi_Minh'),
-                        'updated_at' => Carbon::now('Asia/Ho_Chi_Minh'),
-                    ]);
-            } else {
-                return null;
+                Employee::create([
+                    'name' => $row['name'],
+                    'phoneNumber' => $row['phone_number'],
+                    'dateOfBirth' => $date_format,
+                    'departmentId' => $departmentId,
+                    'sex' => strtolower($row['sex']) === 'nam' ? 1 : 0,
+                    'id' => $admin->id,
+                    'created_at' => Carbon::now('Asia/Ho_Chi_Minh'),
+                    'updated_at' => Carbon::now('Asia/Ho_Chi_Minh'),
+                ]);
             }
-        } else {
-            return null;
         }
     }
 }
